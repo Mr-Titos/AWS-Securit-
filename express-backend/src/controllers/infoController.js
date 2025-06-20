912fd1b107117c33e8039b94f8926ddde2d32d05
Mr Titos <arthur.titos2@gmail.com>
2025-06-20 16:30:01 +0200

    finish the RDS connection

--- express-backend/src/controllers/infoController.js
+++ express-backend/src/controllers/infoController.js
@@ -1,10 +1,26 @@
-const getInfo = (req, res) => {
-    const staticInfo = {
-        name: "Express Backend",
-        version: "1.0.0",
-        description: "A simple Express backend project."
-    };
-    res.json(staticInfo);
+const mysql = require('mysql2/promise');
+
+const dbConfig = {
+      host: 'database-1.cham40qqgjy1.eu-north-1.rds.amazonaws.com',
+      user: 'appuser',
+      password: 'motdepassefort',
+      database: 'db'
+};
+
+const getInfo = async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const [rows] = await connection.execute('SELECT * FROM db.info;');
+    if (rows.length > 0) {
+        res.json(rows[0]);
+    } else {
+        res.status(404).json({ error: 'No info found' });
+    }
+    console.log('Connexion OK:', rows);
+    await connection.end();
+  } catch (err) {
+    console.error('Erreur MySQL:', err);
+  }
 };
 
 module.exports = { getInfo };
